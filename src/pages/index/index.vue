18f77ebbd877bd9f157d841af5d4c0da2a9bb94a
hanyabo <2247561875@qq.com>
2023-11-21 10:51:09 +0800

    首页自定义导航栏自动适配

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-//
+import CustomNavBar from './components/CustomNavbar.vue'
 </script>
 
 <template>
-  <view class="index">index</view>
+  <CustomNavBar></CustomNavBar>
+  <view class="index"></view>
 </template>
 
 <style lang="scss">
