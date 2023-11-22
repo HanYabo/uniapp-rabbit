8dbe67ad01ec7173074510cedac4e6d5bd0b7067
hanyabo <2247561875@qq.com>
2023-11-22 17:18:41 +0800

    小程序模拟快捷登录

---
+++ src/services/login.ts
@@ -0,0 +1,35 @@
+import { http } from "@/utils/http"
+
+// 定义登录数据类型LoginParams
+type LoginParams = {
+  code: string,
+  encryptedData: string,
+  iv: string
+}
+
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
+
+/**
+ * 小程序登录-内测版
+ * @param phoneNumber 模拟手机号码
+ * @returns
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber
+    }
+  })
+}
